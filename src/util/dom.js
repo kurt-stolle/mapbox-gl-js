b9b4c57a5418694b3cb7d1e966e858eb9b4f0f30
Kurt Stolle <k.h.w.stolle@gmail.com>
2020-07-04 12:29:04 +0200

    Fixed scaled dragging issue

--- src/util/dom.js
+++ src/util/dom.js
@@ -104,20 +104,27 @@ DOM.suppressClick = function() {
 };
 
 DOM.mousePos = function (el: HTMLElement, e: MouseEvent | window.TouchEvent | Touch) {
-    const rect = el.getBoundingClientRect();
+    const rect = el.getBoundingClientRect(),
+        factorX = rect.width ? el.offsetWidth / rect.width : 1,
+        factorY = rect.height ? el.offsetHeight / rect.height : 1;
+
+    const t = window.TouchEvent && (e instanceof window.TouchEvent) ? e.touches[0] : e;
+
     return new Point(
-        e.clientX - rect.left - el.clientLeft,
-        e.clientY - rect.top - el.clientTop
+        (t.clientX - rect.left) * factorX - el.clientLeft,
+        (t.clientY - rect.top) * factorY - el.clientTop
     );
 };
 
 DOM.touchPos = function (el: HTMLElement, touches: TouchList) {
     const rect = el.getBoundingClientRect(),
+        factorX = rect.width ? el.offsetWidth / rect.width : 1,
+        factorY = rect.height ? el.offsetHeight / rect.height : 1,
         points = [];
     for (let i = 0; i < touches.length; i++) {
         points.push(new Point(
-            touches[i].clientX - rect.left - el.clientLeft,
-            touches[i].clientY - rect.top - el.clientTop
+            (touches[i].clientX - rect.left) * factorX - el.clientLeft,
+            (touches[i].clientY - rect.top) * factorY - el.clientTop
         ));
     }
     return points;
